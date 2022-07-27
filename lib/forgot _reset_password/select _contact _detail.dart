48fedac044b749e879d1f9116e4cadcb961b61d2
ftesolution <fk236179@gmail.com>
2022-07-26 23:39:53 -0700

    select contact for reset

---
+++ lib/forgot _reset_password/select _contact _detail.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+class Select_Contact extends StatefulWidget {
+  const Select_Contact({Key? key}) : super(key: key);
+
+  @override
+  State<Select_Contact> createState() => _Select_ContactState();
+}
+
+class _Select_ContactState extends State<Select_Contact> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
